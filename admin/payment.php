eaddf2093895481d456cca71c0b9c48a2c2d5117
fina0403 <ai220245@student.uthm.edu.my>
2025-06-15 22:43:26 +0800

    initial commit

--- admin/payment.php
+++ admin/payment.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     include '../config.php';
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,23 +11,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- ✅ UPDATED TITLE FOR TESTING PUSH -->
-    <title>BlueBird - Admin Panel (Updated)</title>
-    
+    <title>BlueBird - Admin</title>
     <!-- boot -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
-    <!-- fontawesome -->
+    <!-- fontowesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    
-    <!-- custom css -->
-    <link rel="stylesheet" href="css/roombook.css">
-</head>
+	<!-- css for table and search bar -->
+	<link rel="stylesheet" href="css/roombook.css">
 
+</head>
 <body>
-    <!-- ✅ THIS IS A TEST COMMENT TO CHECK GITHUB PUSH -->
 	<div class="searchsection">
         <input type="text" name="search_bar" id="search_bar" placeholder="search..." onkeyup="searchFun()">
     </div>
@@ -34,6 +30,7 @@
         <?php
             $paymanttablesql = "SELECT * FROM payment";
             $paymantresult = mysqli_query($conn, $paymanttablesql);
+
             $nums = mysqli_num_rows($paymantresult);
         ?>
         <table class="table table-bordered" id="table-data">
@@ -44,7 +41,7 @@
                     <th scope="col">Room Type</th>
                     <th scope="col">Bed Type</th>
                     <th scope="col">Check In</th>
-                    <th scope="col">Check Out</th>
+                    <th scope="col">Check In</th>
 					<th scope="col">No of Day</th>
                     <th scope="col">No of Room</th>
 					<th scope="col">Meal Type</th>
@@ -53,6 +50,7 @@
                     <th scope="col">Meals</th>
 					<th scope="col">Total Bill</th>
                     <th scope="col">Action</th>
+                    <!-- <th>Delete</th> -->
                 </tr>
             </thead>
 
@@ -75,7 +73,7 @@
 					<td><?php echo $res['mealtotal'] ?></td>
 					<td><?php echo $res['finaltotal'] ?></td>
                     <td class="action">
-                        <a href="invoiceprint.php?id= <?php echo $res['id']?>"><button class="btn btn-primary"><i class="fa-solid fa-print"></i> Print</button></a>
+                        <a href="invoiceprint.php?id= <?php echo $res['id']?>"><button class="btn btn-primary"><i class="fa-solid fa-print"></i>Print</button></a>
 						<a href="paymantdelete.php?id=<?php echo $res['id']?>"><button class="btn btn-danger">Delete</button></a>
                     </td>
                 </tr>
@@ -89,19 +87,29 @@
 
 <script>
     //search bar logic using js
-    const searchFun = () => {
+    const searchFun = () =>{
         let filter = document.getElementById('search_bar').value.toUpperCase();
+
         let myTable = document.getElementById("table-data");
+
         let tr = myTable.getElementsByTagName('tr');
 
-        for (let i = 0; i < tr.length; i++) {
+        for(var i = 0; i< tr.length;i++){
             let td = tr[i].getElementsByTagName('td')[1];
-            if (td) {
+
+            if(td){
                 let textvalue = td.textContent || td.innerHTML;
-                tr[i].style.display = textvalue.toUpperCase().indexOf(filter) > -1 ? "" : "none";
+
+                if(textvalue.toUpperCase().indexOf(filter) > -1){
+                    tr[i].style.display = "";
+                }else{
+                    tr[i].style.display = "none";
+                }
             }
         }
+
     }
+
 </script>
 
 </html>
